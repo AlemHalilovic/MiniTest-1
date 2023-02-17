d33200bc30a362f0da6b0614d2853197a7e3dc6d
Alka04 <Admin@TEB127009>
2023-02-17 10:27:15 +0100

    Add project files.

---
+++ MiniTest 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Minitest_1
+{
+    class progeam
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två heltal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+
+            Area(tal1, tal2);
+            Console.WriteLine($"Arean på ditt föremål kommer då bli {tal1 * tal2} i kvadrat");
+
+
+        }
+
+        static int Area(int tal1, int tal2)
+        {
+            return (tal1 * tal2);
+        }
+    }
+}
